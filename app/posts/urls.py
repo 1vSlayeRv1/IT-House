8257109781309571a8c788f7dbbe93e2508602d8
1vSlayeRv1 <tretyakov.main@yandex.ru>
2021-05-27 14:55:22 +0300

    ID

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import print_posts
+from .views import print_posts, open_post
 urlpatterns = [
+    path('<int:post_id>', open_post, name='open_post'),
     path('', print_posts, name = 'print_posts')
 ]
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,9 +1,15 @@
 import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Profile, Post
+from django.core import serializers
+from .models import Profile, Post, Comment
 
 def print_posts(request):
     posts = list(Post.objects.values())
+    return JsonResponse({'posts': posts})
 
-    return JsonResponse({'posts': posts}, safe=False)
+    
+def open_post(request, post_id):
+    post = list(Post.objects.values().get(pk=post_id)) 
+    comments = list(Comment.objects.values().filter(post_id=post_id))
+    return JsonResponse({'post': post, 'comments': comments})
