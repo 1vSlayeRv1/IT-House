05f87a8c485d51f4296d036b9b351f0df635fa75
1vSlayeRv1 <tretyakov.main@yandex.ru>
2021-06-02 14:11:54 +0300

    Fix

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -3,18 +3,24 @@ from .models import Post,  Comment
 from django.utils.safestring import mark_safe
 from images.models import Image
 
+
 class ImagesInline(admin.TabularInline):
     model = Image.post.through
-    
+    readonly_fields = ['preview']
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image}" style="max-height: 200px;">')
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'date')
     list_display_links = ('title', 'date')
     search_fields = ('title', 'date')
-    readonly_fields = ['preview']
+
     inlines = (ImagesInline, )
-    fileds = ('title', 'description', 'content', 'preview')
-    def preview(self, obj):
-        return mark_safe(f'<img src="{obj.post_image.url}" style="max-height: 200px;">')
+    fileds = ('title', 'description', 'content')
+
+
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('comment', 'date', 'profile_id', 'post_id')
     list_display_links = ('comment', 'profile_id', 'post_id')
